7a96f4231aadacab197c4e73fe2d48cc4deecfc8
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2019-11-06 09:18:38 -0600

    Initial

---
+++ Northwind-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorthwindConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
