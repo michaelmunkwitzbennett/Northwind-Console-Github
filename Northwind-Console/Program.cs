e855fd56a9cbc591a016a403e3084f2b6260b5c0
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2019-11-06 09:51:27 -0600

    Option 1 - Display Categories

--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -17,11 +17,24 @@ namespace NorthwindConsole
                 string choice;
                 do
                 {
-                    Console.Clear();
                     Console.WriteLine("1) Display Categories");
                     Console.WriteLine("2) Add Category");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
+                    Console.Clear();
+                    logger.Info($"Option {choice} selected");
+                    if (choice == "1")
+                    {
+                        var db = new NorthwindContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryName);
+
+                        Console.WriteLine($"{query.Count()} records returned");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+                        }
+                    }
+                    Console.WriteLine();
 
                 } while (choice.ToLower() != "q");
             }
